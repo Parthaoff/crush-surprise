e7288b3c8689fbc2f9f2356798cd9f52d131cdf0
PARTHA SINGHA <145651386+Parthaoff@users.noreply.github.com>
2026-02-04 13:25:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/crush-surprise/", // <--- ADD THIS LINE (use your actual repo name)
-})
+  })
